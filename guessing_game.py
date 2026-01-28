a57474eb22f374ef1c8dd06ec50fc5ea04c8ba0f
Jordan Jones <jordanjonesodu@gmail.com>
2026-01-27 22:12:32 -0500

    refactored guessing game to use functions

--- guessing_game.py
+++ guessing_game.py
@@ -1,21 +1,32 @@
 # Import the random module to generate random numbers
 import random
 
+# Function for user guess
+def get_guess():
+    return int(input("Please Pick a Number: "))
+
+# function to check if guess is too high/too low
+def check_guess(guess, secret):
+    if guess < secret:       # Conditional statements to handle if the user guesses too low or too high
+        print("Your guess is too low! Try again")
+        return False     # wrong guess
+    elif guess > secret:
+        print("Your guess is too high! Try again")
+        return False   # wrong guess
+    else:
+        return True    # correct guess!
+
+
 lucky_number = random.randint(1, 24) # a random number between 1 and 24 is chosen
 
 print("Let's play a game! Try to guess the magic number that I am thinking about!")
 
-
 num_guesses = 0   # initiate a counter for the loop
-user_guess = 0
-while user_guess != lucky_number:       # while loop that keeps going until user guesses correctly
-    user_guess = int(input("Please Pick a Number: "))
+guess = 0
+while guess != lucky_number:       # while loop that keeps going until user guesses correctly
+    guess = get_guess()            # Get a number from the user
+    check_guess(guess, lucky_number)
     num_guesses += 1
-
-    if user_guess < lucky_number:       # Conditional statements to handle if the user guesses too low or too high
-        print("Your guess is too low! Try again")
-    elif user_guess > lucky_number:
-        print("Your guess is too high! Try again")
 
 print("That's the right number! You won!")
 print(f"It took you {num_guesses} guesses to win the game!")
