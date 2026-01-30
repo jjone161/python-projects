089e45f666956c08e6acd8c821178b58b2b45c5a
Jordan Jones <jordanjonesodu@gmail.com>
2026-01-29 22:16:44 -0500

    created a timer app

---
+++ timer.py
@@ -0,0 +1,10 @@
+# let's build a timer app
+
+# Get a number as input from the user
+user_num = int(input("Please choose a number: "))
+
+# for loop to loop through number and count down
+for num in range(user_num, 0, -1):
+    print(num)     # prints the countdown
+
+print("Blastoff!!!!")   # print blastoff!
