574fe68dfabdb151efc2569bed6b78ceb2bb5622
Jordan Jones <jordanjonesodu@gmail.com>
2026-01-29 22:03:32 -0500

    Revert guessing_game.py to previous version

--- guessing_game.py
+++ guessing_game.py
@@ -1,35 +1,33 @@
-# import random module to generate numbers at random
+# Import the random module to generate random numbers
 import random
 
-# pick a random number between 1 and 23
-lucky_number = random.randint(1, 23)
-
-num_guesses = 0
-user_guess = 0
-
-# create a function for the user to guess a numnber
+# Function for user guess
 def get_guess():
-   return (int(input("Please select a number: ")))
+    return int(input("Please Pick a Number: "))
 
-# create a function for whether a guess is high, low, or correct
+# function to check if guess is too high/too low
 def check_guess(guess, secret):
-   if guess < secret:
-      print("Number is too low! Try again! ")
-      return False
-   elif guess > secret:
-      print("Number is too high! Try again! ")
-      return False
-   else:
-      print("You chose the correct number! Congrats!")
-      print(f" you won the game in {num_guesses} guesses")
-      return True
-# While look to continue the game until user wins
-while user_guess != lucky_number:
-   user_guess = get_guess()
-   check_guess(user_guess, lucky_number)
-   num_guesses += 1
+    if guess < secret:       # Conditional statements to handle if the user guesses too low or too high
+        print("Your guess is too low! Try again")
+        return False     # wrong guess
+    elif guess > secret:
+        print("Your guess is too high! Try again")
+        return False   # wrong guess
+    else:
+        return True    # correct guess!
 
 
+lucky_number = random.randint(1, 24) # a random number between 1 and 24 is chosen
 
-      
-   
+print("Let's play a game! Try to guess the magic number that I am thinking about!")
+
+num_guesses = 0   # initiate a counter for the loop
+guess = 0
+while guess != lucky_number:       # while loop that keeps going until user guesses correctly
+    guess = get_guess()            # Get a number from the user
+    check_guess(guess, lucky_number)
+    num_guesses += 1
+
+print("That's the right number! You won!")
+print(f"It took you {num_guesses} guesses to win the game!")
+
